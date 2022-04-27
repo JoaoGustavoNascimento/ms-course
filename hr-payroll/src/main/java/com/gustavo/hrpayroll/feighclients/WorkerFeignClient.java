cb871952a64959afe86353901eea2abbcd67af41
Joao Gustavo <gustavojoao28@gmail.com>
2022-04-27 17:02:40 -0300

    Feign client

---
+++ hr-payroll/src/main/java/com/gustavo/hrpayroll/feighclients/WorkerFeignClient.java
@@ -0,0 +1,18 @@
+package com.gustavo.hrpayroll.feighclients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.gustavo.hrpayroll.entities.Worker;
+
+@Component
+@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+public interface WorkerFeignClient {
+	
+	@GetMapping(value = "/{id}")
+	ResponseEntity<Worker> findAllById(@PathVariable Long id);
+
+}
